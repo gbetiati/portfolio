af22ecb407ee793dd94d2659f3aa6d420c3fcd7e
˜gbetiati˜ <guilherme_betiati@hotmail.com>
2024-09-18 22:02:50 -0300

    deplo y configs v3

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+
+const Contact = () => {
+
+  
+  return (
+    <div className="h-full w-full">
+        <div className="text text-5xl text-center text-green-400">
+            Em construçao...
+        </div>
+    </div>
+  );
+};
+
+export default Contact;
